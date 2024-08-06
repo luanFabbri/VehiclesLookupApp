4524487a233907f7669d6b337edb486d5af8b0c6
LuanF <luan.fabbri@hotmail.com>
2024-08-05 23:20:34 -0300

    v0.1 - Inicialização do projeto

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+// src/i18n.ts
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(HttpApi)
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
+  });
+
+export default i18n;
---
+++ src/services/api-config.ts
@@ -0,0 +1,16 @@
+// src/api.ts
+export const login = async (email: string, password: string) => {
+  const response = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({email, password}),
+  });
+
+  if (!response.ok) {
+    throw new Error('Login failed');
+  }
+
+  return response.json();
+};
