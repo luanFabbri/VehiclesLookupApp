7e90f5860c63ec40a87b910ea1621ebdb853f446
LuanF <luan.fabbri@hotmail.com>
2024-08-13 01:45:14 -0300

    teste

--- android/app/src/main/java/com/ituranmobtechinterviewapp/MainApplication.kt
+++ android/app/src/main/java/com/ituranmobtechinterviewapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ituranmobtechinterviewapp
+package com.VehiclesLookupMockup
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/VehiclesLookupMockupTests/VehiclesLookupMockupTests.m
+++ ios/VehiclesLookupMockupTests/VehiclesLookupMockupTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface IturanmobTechInterviewAppTests : XCTestCase
+@interface VehiclesLookupMockupTests : XCTestCase
 
 @end
 
-@implementation IturanmobTechInterviewAppTests
+@implementation VehiclesLookupMockupTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
